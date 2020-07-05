a4e6dc3d9a55338c1d292b6b85a9c92c3c31dec0
Lucas Royland <roylucas@gmail.com>
2020-07-05 10:55:20 -0700

    Added Property Lead Form project. Basic runserver functions.

---
+++ PropertyLeadForm/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PropertyLeadForm project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PropertyLeadForm.settings')
+
+application = get_asgi_application()
